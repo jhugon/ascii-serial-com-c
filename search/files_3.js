7c2a83af3f1eae229549b428e3dc129ee103d21b
jhugon <jhugon@users.noreply.github.com>
2022-02-01 00:02:49 +0000

    deploy: 92330f7011a40751690a166dfd559a56cdd8247f

--- search/files_3.js
+++ search/files_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['neorv32_5fasc_5fgpio_2ec_0',['neorv32_asc_gpio.c',['../neorv32__asc__gpio_8c.html',1,'']]],
-  ['neorv32_5fblink_5fled_2ec_1',['neorv32_blink_led.c',['../neorv32__blink__led_8c.html',1,'']]],
-  ['neorv32_5fchar_5floopback_2ec_2',['neorv32_char_loopback.c',['../neorv32__char__loopback_8c.html',1,'']]]
+  ['neorv32_5fasc_5fpulse_5fcounter_2ec_1',['neorv32_asc_pulse_counter.c',['../neorv32__asc__pulse__counter_8c.html',1,'']]],
+  ['neorv32_5fblink_5fled_2ec_2',['neorv32_blink_led.c',['../neorv32__blink__led_8c.html',1,'']]],
+  ['neorv32_5fchar_5floopback_2ec_3',['neorv32_char_loopback.c',['../neorv32__char__loopback_8c.html',1,'']]]
 ];
