60df6dcdfa36ae1b19459298958c9a0fa7a2208d
Justin Hugon <jhugon@users.noreply.github.com>
2023-02-27 20:55:49 -0600

    updating avr/avr_uart.h: in progress
    not compiling, in progress

--- avr/arduino_uno_char_loopback.c
+++ avr/arduino_uno_char_loopback.c
@@ -1,3 +1,4 @@
+#include "asc_helpers.h"
 #include "avr/avr_uart.h"
 #include <avr/io.h>
 
@@ -7,17 +8,28 @@
 //#define BAUD 9600
 #define BAUD 19200
 #define MYUBRR (FOSC / 16 / BAUD - 1)
+#define UART_NO 0
 
 uint8_t byteBuffer;
 
 int main(void) {
 
-  USART0_Init(MYUBRR, 0);
+  UART_Init(UART_NO, MYUBRR, 0);
 
   while (1) {
-    USART0_Rx(byteBuffer);
+    while (1) {
+      if (is_uart_rx_data_waiting(UART_NO)) {
+        uart_rx(UART_NO, byteBuffer);
+        break;
+      }
+    }
     //_delay_ms(100);
-    USART0_Tx(byteBuffer);
+    while (1) {
+      if (is_uart_ready_to_tx(UART_NO)) {
+        uart_tx(UART_NO, byteBuffer);
+        break;
+      }
+    }
   }
   return 0;
 }
